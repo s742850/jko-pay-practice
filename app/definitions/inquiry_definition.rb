635a610d64f37bb24c2b66bb68b6be420bbdf842
daniel <daniel@weibyapps.com>
2021-08-19 11:11:20 +0800

    新增inquiry_type值檢查

--- app/definitions/inquiry_definition.rb
+++ app/definitions/inquiry_definition.rb
@@ -1,7 +1,7 @@
 RequestParamsValidation.define do
   action :create do
     request do
-      required :inquiry_type, type: :string, length: { max: 1 }
+      required :inquiry_type, type: :string, length: { max: 1 }, validate: lambda { |value| ["R", "P"].include?(value) }
       required :merchant_id, type: :string, length: { max: 10 }
       required :merchant_key, type: :string
       required :merchant_trade_number, type: :string, length: { max: 60 }
