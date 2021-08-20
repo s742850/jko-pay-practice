9097812b950a17923037931cd031bd7a2bb8c4b2
daniel <daniel@weibyapps.com>
2021-08-21 07:26:17 +0800

    refactor method name and fix style/hashes

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -4,13 +4,13 @@ class StoreController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    render :json => { status_code: ::Response::StatusCode::SUCCESS, stores: Store.all }
+    render json: { status_code: ::Response::StatusCode::SUCCESS, stores: Store.all }
   end
 
   def create
     store = Store.new
     store.valid?
-    assignParams(store)
+    assign_params(store)
     store_save_and_response(store)
   end
 
@@ -18,34 +18,34 @@ class StoreController < ApplicationController
     # catch or find_by
     store_find = Store.find_by_id(params[:id])
     if store_find
-      render :json => { status_code: ::Response::StatusCode::SUCCESS, stores: store_find }
+      render json: { status_code: ::Response::StatusCode::SUCCESS, stores: store_find }
     else
-      render :json => { status_code: ::Response::StatusCode::ERROR_ROW_NOT_FOUND }
+      render json: { status_code: ::Response::StatusCode::ERROR_ROW_NOT_FOUND }
     end
   end
 
   def update
     store_find = Store.find(params[:id])
-    assignParams(store_find)
+    assign_params(store_find)
     store_save_and_response(store_find)
   end
 
   def delete
     store_find = Store.find(params[:id])
-    render :json => { status_code: ::Response::StatusCode::SUCCESS } if store_find.delete
+    render json: { status_code: ::Response::StatusCode::SUCCESS } if store_find.delete
   end
 
   private
 
   def store_save_and_response(store)
     if store.save
-      render :json => { status_code: ::Response::StatusCode::SUCCESS }
+      render json: { status_code: ::Response::StatusCode::SUCCESS }
     else
-      render :json => { status_code: ::Response::StatusCode::ERROR_DB }
+      render json: { status_code: ::Response::StatusCode::ERROR_DB }
     end
   end
 
-  def assignParams(store)
+  def assign_params(store)
     store.merchant_id = params[:merchant_id] if params[:merchant_id]
     store.store_id = params[:store_id] if params[:store_id]
     store.nstore_idame = params[:name] if params[:name]
