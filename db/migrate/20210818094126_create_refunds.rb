4400837b17112fb942448cbc89594a6ed7e9e8a0
daniel <daniel@weibyapps.com>
2021-08-25 10:06:18 +0800

    add unique index

--- db/migrate/20210818094126_create_refunds.rb
+++ db/migrate/20210818094126_create_refunds.rb
@@ -4,7 +4,7 @@ class CreateRefunds < ActiveRecord::Migration[6.1]
       t.string :merchant_id, comment: '特店代碼,每家商店都不一樣', null: false
       t.string :store_id, comment: '商店編號', null: false
       t.string :gateway_trade_no, comment: '銀行端交易序號', null: true
-      t.string :merchant_trade_no, comment: '商店端交易序號,唯一值', null: false, :index => true
+      t.string :merchant_trade_no, comment: '商店端交易序號,唯一值', null: false, index: true, unique: true
       t.string :pos_id, comment: 'POS機號', null: false
       t.integer :trade_amount, comment: '退款金額', null: false
       t.integer :debit_amount, comment: '付款方式退款金額', null: false
